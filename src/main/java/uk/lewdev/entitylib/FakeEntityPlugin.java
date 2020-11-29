1208de92b69421c7bdbb0fca0a3bea3c686306ef
Lewys Davies <lewys.davies@btinternet.com>
2020-11-29 17:11:51 +0000

    Update main plugin class

--- src/main/java/uk/lewdev/entitylib/FakeEntityPlugin.java
+++ src/main/java/uk/lewdev/entitylib/FakeEntityPlugin.java
@@ -1,15 +1,12 @@
 package uk.lewdev.entitylib;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import uk.lewdev.entitylib.entity.protocol.EntityAsyncRenderTicker;
-import uk.lewdev.entitylib.events.ProtocolLibListeners;
 
 /**
  * @author Lewys Davies (Lew_)
  */
-public class FakeEntityPlugin extends JavaPlugin implements Listener {
+public class FakeEntityPlugin extends JavaPlugin {
 
 	public static FakeEntityPlugin instance;
 	
@@ -17,9 +14,7 @@ public class FakeEntityPlugin extends JavaPlugin implements Listener {
 	public void onEnable() {
 		instance = this;
 		
-		Bukkit.getPluginManager().registerEvents(this, this);
-		
 		new EntityAsyncRenderTicker().runTaskTimerAsynchronously(this, 20, 20);
-		new ProtocolLibListeners();
+		//new ProtocolLibListeners();
 	}
 }
